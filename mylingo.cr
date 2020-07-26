9c4b163ade4eb1fd9b2dd243f49dedaba21e64e5
Dave Goddard <dave@goddard.id.au>
2020-07-26 13:20:04 +1000

    generates classes ; missing types

--- mylingo.cr
+++ mylingo.cr
@@ -4,7 +4,7 @@ class QueryParser < Lingo::Parser
   rule(:whitespace) { match(/\s+/) }
   rule(:name) { match(/[a-z][a-zA-Z]*/) }
   rule(:args) { str("(") >> match(/[^)]*/) >> str(")") }
-  rule(:field) { name.named(:field_name) >> args.named(:field_args).maybe >> whitespace.maybe >> nested.maybe }
+  rule(:field) { name.named(:field_name) >> args.maybe >> whitespace.maybe >> nested.named(:nested).maybe }
   rule(:field_whitespace) { field >> whitespace.maybe }
   rule(:nested) { str("{") >> whitespace.maybe >> field_whitespace.repeat.maybe >> whitespace.maybe >> str("}") }
   rule(:query) { str("query") >> whitespace.maybe >> args.maybe}
@@ -12,7 +12,40 @@ class QueryParser < Lingo::Parser
   root(:full)
 end
 
-pp QueryParser.new.parse(%{
+class QueryVisitor < Lingo::Visitor
+  # Set up an accumulator
+  getter :path, :prefix, :code
+  setter :prefix, :code
+  def initialize
+    @path = [""] of String
+    @prefix = "  "
+    @code = ""
+  end
+
+  def code_str
+    "class Outer\n#{code}end\n"
+  end
+
+  enter(:field_name) {
+    # puts "ENTER: #{node.full_value}"
+    visitor.code += "#{visitor.prefix}property #{node.full_value.to_s.underscore}\n"
+    visitor.path[visitor.path.size - 1] = node.full_value.to_s
+  }
+
+  enter(:nested) {
+    visitor.code += "\n#{visitor.prefix}class #{visitor.path.last.camelcase}\n#{visitor.prefix}  include JSON::Serializable\n"
+    visitor.path << ""
+    visitor.prefix += "  "
+  }
+
+  exit(:nested) {
+    visitor.prefix = visitor.prefix.chomp("  ")
+    visitor.code += "#{visitor.prefix}end\n"
+    visitor.path.pop
+  }
+end
+
+ast = QueryParser.new.parse(%{
   query($first: Int) {
     advertSearch {
       adverts(first: $first) {
@@ -36,3 +69,6 @@ pp QueryParser.new.parse(%{
     }
   }
 })
+visitor = QueryVisitor.new
+visitor.visit(ast)
+puts "=====\n#{visitor.code_str}\n"
