889cc9087930d48457e7bb8d465906dc31469df6
Dave Goddard <dave@goddard.id.au>
2020-07-26 12:37:32 +1000

    wip

--- mylingo.cr
+++ mylingo.cr
@@ -4,8 +4,9 @@ class QueryParser < Lingo::Parser
   rule(:whitespace) { match(/\s+/) }
   rule(:name) { match(/[a-z][a-zA-Z]*/) }
   rule(:args) { str("(") >> match(/[^)]*/) >> str(")") }
-  rule(:field) { name >> args.maybe >> whitespace.maybe >> nested.maybe }
-  rule(:nested) { str("{") >> whitespace.maybe >> field.repeat.maybe >> whitespace.maybe >> str("}") }
+  rule(:field) { name.named(:field_name) >> args.named(:field_args).maybe >> whitespace.maybe >> nested.maybe }
+  rule(:field_whitespace) { field >> whitespace.maybe }
+  rule(:nested) { str("{") >> whitespace.maybe >> field_whitespace.repeat.maybe >> whitespace.maybe >> str("}") }
   rule(:query) { str("query") >> whitespace.maybe >> args.maybe}
   rule(:full) { whitespace.maybe >> query >> whitespace.maybe >> nested.maybe >> whitespace.maybe}
   root(:full)
@@ -19,30 +20,19 @@ pp QueryParser.new.parse(%{
           hasNextPage
           endCursor
         }
+        nodes {
+          id
+          title
+          variants {
+            nodes {
+              id
+              label
+              barcode
+              countOnHand
+            }
+          }
+        }
       }
     }
   }
 })
-
-# pp QueryParser.new.parse(%{query($first: Int) {
-#     advertSearch {
-#       adverts(first: $first) {
-#         pageInfo {
-#           hasNextPage
-#         }
-#         nodes {
-#           id
-#           title
-#           variants {
-#             nodes {
-#               id
-#               label
-#               barcode
-#               countOnHand
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# })
